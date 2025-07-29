0cce5aca3fbb25926cee7b798f6e5341bce81abc
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-07-28 18:52:06 -0700

    add more code

--- wsgi_webserver.py
+++ wsgi_webserver.py
@@ -28,4 +28,43 @@ class WSGIServer(object):
         host, port = self.listen_socket.getsockname()[:2]
         self.server_name = socket.getfqdn(host)
         self.server_port = port
-        
+
+        # return headers set by web framework/web app
+        self.headers_set = []
+
+    
+    def set_app(self, app):
+        self.app = app
+    
+
+    def serve_forever(self):
+        listen_socket = self.listen_socket
+        while True:
+            # new client connection
+            self.client_connection, client_address = listen_socket.accept()
+
+            # handle one request and close the client connection
+            # then loop over to wait for another client connection
+            self.handle_one_request()
+    
+
+    def handle_one_request(self):
+        request_data = self.client_connection.recv(1024)
+        self.request_data = request_data = request_data.encode('utf-8')
+
+        # print formatted request data a la 'curl -v'
+        print(''.join(
+            f'< {line}\n' for line in request_data.splitlines()
+        ))
+
+        self.parse_request(request_data)
+
+        # construct env dict using request data
+        env = self.get_environ()
+    
+        # time to call our app callable and
+        # get back result that will become HTTP resp body
+        result = self.app(env, self.start_response)
+
+        # construct response and send it back to client
+        self.finish_response(result)
